07574bb2a2500968621181b7521755a680205dc6
Nino Rumi <nino.rumi@outlook.de>
2017-10-12 11:22:12 +0200

    Update für LUIS Connection

--- NeueChatbot/server.js
+++ NeueChatbot/server.js
@@ -1,8 +1,37 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+var restify = require('restify');
+var builder = require('botbuilder');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
+
+// You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
+// This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+
+bot.dialog('Help', function (session) {
+    session.endDialog('Hallo ich bin der freundliche CSS Bot und ich werde dir helfen herauszufinden wie es um die Rückerstattung deines Fitness abos steht');
+}).triggerAction({
+    matches: 'Help'
+});
+
+
+
+
