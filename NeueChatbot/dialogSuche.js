b12df27a528cb00c52fed79237a2cb68827eafdc
Nino Rumi <nino.rumi@outlook.de>
2017-10-13 17:32:35 +0200

    Dialog Umgestellt

--- NeueChatbot/dialogSuche.js
+++ NeueChatbot/dialogSuche.js
@@ -12,15 +12,18 @@ module.exports = {
         var sendMsg =  function (s, i, o) {
             session.send('%s, %s', s.name, s.website);
         }
-
-        fetch('http://35.189.74.56/fCenters/search/findByLocationContainingIgnoreCase?location=' + locationEntity.entity.toString())
-            .then(response => response.json())
-            .then(data => {
-                data._embedded.fCenters.forEach(sendMsg);
+        if (locationEntity) {
+            fetch('http://35.189.74.56/fCenters/search/findByLocationContainingIgnoreCase?location=' + locationEntity.entity.toString())
+                .then(response => response.json())
+                .then(data => {
+                    data._embedded.fCenters.forEach(sendMsg);
                 }).catch(err => {
-                console.error('An error ocurred', err);
-            });
+                    console.error('An error ocurred', err);
+                });
+        } else {
+            session.endDialog('Ich habe leider nicht verstanden wo sie ein Fitness Center suchen');
+        }
 
-        session.endDialog('Wo möchten Sie ein Fitnessabo abschliessen' + locationEntity.entity.text);
+        
     }
 }
