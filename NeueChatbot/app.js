f2930083387ac72fdfe1cb2209ba06118ed935de
Yu Li <yu.li@ipt.ch>
2017-10-18 17:33:57 +0200

    Integration with API von Nino (Springboot) and user context

--- NeueChatbot/app.js
+++ NeueChatbot/app.js
@@ -74,15 +74,16 @@ bot.dialog('Login',
         },
         function (session, results) {
             session.conversationData['Username'] = results.response.entity;
+            
 
             var restURL = "http://chatbotsandbox.getsandbox.com/v1.0/product/" + session.conversationData['Username'];
-
-            console.log('URL:', restURL);
 
             fetchUrl(restURL, function (error, meta, body) {
                 var obj = JSON.parse(body);
 
-                console.log('Error downloading attachment:', String(obj.type));
+                session.conversationData['UserInsureType'] = String(obj.type);
+                session.conversationData['UserProductType'] = String(obj.product);
+                session.conversationData['UserDiscount'] = String(obj.discount);
 
                 session.endDialog("Hallo " + session.conversationData['Username'] + "! Du hast " + String(obj.type) + " und den Produkt " + String(obj.product));
             });
--- NeueChatbot/dialogeLeistung.js
+++ NeueChatbot/dialogeLeistung.js
@@ -3,12 +3,14 @@ var builder = require('botbuilder');
 
 var fetchUrl = require("fetch").fetchUrl;
 
+var querystring = require('querystring');
+
 module.exports = {
     Leistungsabfrage: function (session, args, next) {
-        var fitnessCenterEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'FitnessCenter');
+        var fitnessCenterEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'FitnessCenterList');
 
         if (fitnessCenterEntity) {
-            session.conversationData['FitnessCenter'] = fitnessCenterEntity.entity;
+            session.conversationData['FitnessCenter'] = fitnessCenterEntity.resolution.values[0];
             checkCert(session)
         } else {
             session.beginDialog('FitnessZentrumFragen');
@@ -30,10 +32,14 @@ module.exports = {
 
     Versicherungstyp: [
         function (session, args, next) {
-            builder.Prompts.choice(session, "Welche Versicherungsprodukte hast du?", "Ohne Zusatzversicherung|Mit Zusatzversicherung", { listStyle: builder.ListStyle.button });
+            if (typeof session.conversationData['Username'] == "undefined") {
+                builder.Prompts.choice(session, "Welche Versicherungsprodukte hast du?", "Grundversicherung|Zusatzversicherung", { listStyle: builder.ListStyle.button });
+            } else {
+                checkCertAndInsure(session)
+            }
         },
         function (session, results) {
-            session.conversationData['Versicherungstyp'] = results.response;
+            session.conversationData['UserInsureType'] = results.response.entity;
             checkCertAndInsure(session)
             session.endDialog();
         }
@@ -41,29 +47,31 @@ module.exports = {
 }
 
 function checkCertAndInsure(session) {
-    var restURL = "http://chatbotsandbox.getsandbox.com/v1.0/fitnesses/" + session.conversationData['FitnessCenter'];
+    var restURL = "http://35.189.74.56/fCenters/search/findByName?name=" + querystring.escape(session.conversationData['FitnessCenter']);
 
     fetchUrl(restURL, function (error, meta, body) {
         var obj = JSON.parse(body);
 
-        if (!JSON.parse(String(obj.certified).toLowerCase())) {
+        if (obj._embedded.fCenters.length == 0) {
             session.send("Tut mir leid! Ihr Fitnessabo wird nicht bezahlt, weil Ihr Fitnesszentrum nicht zertifziert ist.");
-        } else if (session.conversationData['Versicherungstyp'].entity == 'Ohne Zusatzversicherung') {
+        } else if (session.conversationData['UserInsureType'] == 'Grundversicherung') {
             session.send("Tut mir leid! Ihr Fitnessabo wird nicht bezahlt, weil Sie keine Zusatzversicherung haben");
+        } else if (session.conversationData['UserProductType'] == 'Balance' || session.conversationData['UserProductType'] == 'Premium') {
+            session.send("Wir übernehmen " + session.conversationData['UserDiscount'] + " CHF pro Kalenderjahr für Ihr Fitnessabo");
         } else {
-            session.send("Ihr Fitnessabo wird bezahlt!");
+            session.send("Wir übernehmen ein Teil von Ihrem Fitnessabo");
         }
     });
 }
 
 function checkCert(session) {
-    var restURL = "http://chatbotsandbox.getsandbox.com/v1.0/fitnesses/" + session.conversationData['FitnessCenter'];
+    var restURL = "http://35.189.74.56/fCenters/search/findByName?name=" + querystring.escape(session.conversationData['FitnessCenter']);
 
     fetchUrl(restURL, function (error, meta, body) {
         var obj = JSON.parse(body);
 
-        if (!JSON.parse(String(obj.certified).toLowerCase())) {
-            session.send("Tut mir leid! Ihr Fitnessabo wird nicht bezahlt, weil Ihr Fitnesszentrum nicht zertifziert ist.");
+        if (obj._embedded.fCenters.length == 0) {
+            session.send("Tut mir leid! Ihr Fitnessabo wird nicht bezahlt, weil Ihr Fitnesszentrum " + session.conversationData['FitnessCenter'] + " nicht zertifziert ist.");
             session.endDialog();
         } else {
             session.beginDialog('Versicherungstyp');
